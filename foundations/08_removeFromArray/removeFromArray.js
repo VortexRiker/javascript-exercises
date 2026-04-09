f1d343d8c141a1f465235c9e3b3b8cb774a1f303
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-04-09 15:13:46 +0500

    Update array filtering code

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) 
+{
+    // if (valuesToRemove.length === 0)
+    // {
+    //     return array;
+    // }
+    // for (value of valuesToRemove)
+    // {
+    //     let valueIndex = array.indexOf(value);
+    //     while (valueIndex != -1)
+    //     {
+    //         array.splice(valueIndex, 1);
+    //         valueIndex = array.indexOf(value);
+    //     }
+    // }
+    // return array;
+    return array.filter((item)=>!valuesToRemove.includes(item));
 };
 
 // Do not edit below this line
