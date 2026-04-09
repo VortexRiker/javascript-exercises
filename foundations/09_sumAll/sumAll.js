7a5e0c971d0593c5df79ba0c01d19f4d3fe15443
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-04-09 17:36:43 +0500

    Add sumAll solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(lhs, rhs)
+{
+    if ( !Number.isInteger(lhs) || !Number.isInteger(rhs))
+    {
+        return "ERROR";
+    }
 
+    if ( lhs < 0 || rhs < 0)
+    {
+        return "ERROR";
+    }
+
+    const min = lhs < rhs ? lhs : rhs;
+    const max = min === lhs ? rhs : lhs;
+
+    let numbers = [];
+    for (let i = min; i <= max; ++i)
+    {
+        numbers.push(i);
+    }
+    let result = numbers.reduce((sum, number) => sum + number, 0);
+    return result;
 };
 
 // Do not edit below this line
