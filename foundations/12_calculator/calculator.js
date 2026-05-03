d1f4936a444a0bb3e8e26f2d5d20c28b34422f68
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-05-03 21:10:17 +0500

    Add calculator solution

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(lhs, rhs) 
+{
+	return lhs + rhs;
 };
 
-const subtract = function() {
-	
+const subtract = function(lhs, rhs) 
+{
+	return lhs - rhs;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) 
+{
+  return numbers.reduce((sum, number) => add(sum, number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) 
+{
+  return numbers.reduce((result, number) => result * number);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) 
+{
+  let result = 1;
+  if (exponent === 0)
+  {
+    return result;
+  }
+
+  for(let i = 0; i < exponent; ++i)
+  {
+    result *= base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) 
+{
+	// let numbers = [];
+  // for (let i = 1; i <= number; ++i)
+  // {
+  //   numbers.push(i);
+  // }
+  // return multiply(numbers);
+  if( number <= 1)
+  {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
