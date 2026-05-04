362bc09fd8360a40852d1eac5cee1487334b7867
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-05-04 23:09:10 +0500

    Add findTheOldest solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+function getAge(person)
+{
+    let lastYear = person.yearOfDeath;
+    if(!lastYear)
+    {
+        lastYear = (new Date()).getFullYear();
+    }
 
+    return lastYear - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) 
+{
+    people.sort((lhs, rhs) => getAge(lhs) - getAge(rhs));
+
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
