1a294537ce9ee0f8f73d0f5a807e1547d37e134f
VortexRiker <18361963+VortexRiker@users.noreply.github.com>
2026-05-04 22:20:47 +0500

    Add palindrome solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) 
+{
+    phrase = phrase.toLowerCase();
+    let formated = "";
+    const punctuation = ['.',',','!','?','"',"'",'`',';',':'," "];
+    for (let i = 0; i < phrase.length; ++i)
+    {
+        if (punctuation.includes(phrase[i]))
+        {
+            continue;
+        }
+        formated += phrase[i];
+    }
+    const inverseFormated = formated.split("").reverse().join("");
+    return formated === inverseFormated;
 };
 
 // Do not edit below this line
